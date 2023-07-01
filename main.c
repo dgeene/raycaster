cc66cbe7cee0275d5d7f300c889fb07d704fed63
anon <dgeene>
2023-07-01 16:59:17 -0400

    make a pixel movable on screen via wasd keys

--- main.c
+++ main.c
@@ -6,14 +6,34 @@ Simple raycaster
 #include <GL/glu.h>
 #include <GL/glut.h>
 
+float px, py; // player position
+
+void drawPlayer() {
+    glColor3f(1, 1, 0);
+    glPointSize(8);
+    glBegin(GL_POINTS);
+    glVertex2i(px, py);
+    glEnd();
+}
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    drawPlayer();
     glutSwapBuffers();
+}
+
+void buttons(unsigned char key, int x, int y) {
+    if (key == 'a') { px-=5; }
+    if (key == 'd') { px+=5; }
+    if (key == 'w') { py-=5; }
+    if (key == 's') { py+=5; }
+    glutPostRedisplay();
 }
 
 void init() {
     glClearColor(0.3, 0.3, 0.3, 0);
     gluOrtho2D(0, 1024, 512, 0);
+    px=300; py=300; // init players position here
 }
 
 int main(int argc, char** argv) { 
@@ -23,6 +43,7 @@ int main(int argc, char** argv) {
     glutCreateWindow("OpenGL");
     init();
     glutDisplayFunc(display);
+    glutKeyboardFunc(buttons);
     glutMainLoop();
     //return 0;
 }
